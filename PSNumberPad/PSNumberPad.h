50620d2dc34b7afb135977c866d8d7dea68fb56e
Sheng Pan <pansheng@youzan.com>
2017-10-20 14:02:19 +0800

    [Update]

--- PSNumberPad/PSNumberPad.h
+++ PSNumberPad/PSNumberPad.h
@@ -3,7 +3,7 @@
 //  numpad
 //
 //  Created by Pan on 15/9/15.
-//  Copyright (c) 2015年 Insigma Hengtian Software Co., LTD. All rights reserved.
+//  Copyright (c) 2015年 PPPan. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
